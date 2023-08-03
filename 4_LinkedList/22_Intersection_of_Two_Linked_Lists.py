c204f3ef4af3cf589e06647bcd3310da805e3c3d
glowfi <86939591+glowfi@users.noreply.github.com>
2023-08-04 05:19:00 +0530

    added a code

--- 4_LinkedList/22_Intersection_of_Two_Linked_Lists.py
+++ 4_LinkedList/22_Intersection_of_Two_Linked_Lists.py
@@ -24,3 +24,44 @@ class Solution:
             ptr = ptr.next
 
         return None
+
+
+# Optimal
+# T.C. -> O(n1)+O(n2)+O(n2-n1)+O(n2) [Assuming n2 is largest]
+# S.C. -> O(1)
+
+
+class Solution:
+    def getLen(self, head):
+        ptr = head
+        c = 0
+        while ptr:
+            ptr = ptr.next
+            c += 1
+        return c
+
+    def getIntersectionNode(
+        self, headA: ListNode, headB: ListNode
+    ) -> Optional[ListNode]:
+        a, b = self.getLen(headA), self.getLen(headB)
+
+        diff = abs(a - b)
+        p1, p2 = headA, headB
+
+        # Determine the greatest among A and B
+        if a <= b:
+            # Move B diff places
+            for i in range(diff):
+                p2 = p2.next
+        else:
+            # Move A diff places
+            for i in range(diff):
+                p1 = p1.next
+
+        while p1 and p2:
+            if p1 == p2:
+                return p1
+            p1 = p1.next
+            p2 = p2.next
+
+        return None
