2b907b6060cc64981d4e533a112f5676e97443f1
glowfi <86939591+glowfi@users.noreply.github.com>
2023-08-27 00:36:09 +0530

    added more linked list programs

--- 4_LinkedList/31_Flatten_LL.py
+++ 4_LinkedList/31_Flatten_LL.py
@@ -1,6 +1,69 @@
 #  https://www.codingninjas.com/studio/problems/flatten-a-linked-list_1112655 , Medium
 
+# Brute
+# k-> Total no of nodes
+# T.C. -> O(k)+O(klog(k))+O(k)
+# S.C. -> O(k)
 
-# Better
-# T.C. -> O()
-# S.C. -> O()
+
+def flattenLinkedList(head: Node) -> Node:
+    ls = []
+
+    ptr = head
+
+    while ptr:
+        ls.append(ptr.data)
+        tmp = ptr.child
+
+        while tmp:
+            ls.append(tmp.data)
+            tmp = tmp.child
+
+        ptr = ptr.next
+
+    ls.sort()
+
+    dummy = curr = Node(-1)
+    for i in ls:
+        dummy.child = Node(i)
+        dummy = dummy.child
+
+    return curr.child
+
+
+# Optimal
+# T.C. -> O(n*n*k)
+# S.C. -> O(n*k)
+
+
+def merge2sortedlists(l1, l2):
+    p1, p2 = l1, l2
+    tmp = curr = Node(-1)
+
+    while p1 and p2:
+        if p1.data <= p2.data:
+            tmp.child = p1
+            p1 = p1.child
+            tmp = tmp.child
+        else:
+            tmp.child = p2
+            p2 = p2.child
+            tmp = tmp.child
+
+    if p1:
+        tmp.child = p1
+
+    if p2:
+        tmp.child = p2
+
+    return curr.child
+
+
+def flattenLinkedList(head: Node) -> Node:
+    if head is None or head.next is None:
+        return head
+
+    l1 = flattenLinkedList(head.next)
+    l2 = head
+
+    return merge2sortedlists(l2, l1)
--- 4_LinkedList/33_Rotate_LL.py
+++ 4_LinkedList/33_Rotate_LL.py
@@ -1,6 +1,86 @@
 #  https://leetcode.com/problems/rotate-list/ , Medium
 
 
-# Better
-# T.C. -> O()
-# S.C. -> O()
+# Brute
+# T.C. -> O(n*k)
+# S.C. -> O(1)
+
+
+class Solution:
+    def rotateByOne(self, head):
+        ptr = cp = head
+        prev = ptr
+
+        while True:
+            if ptr.next is None:
+                save = prev.next
+                save.next = cp
+                prev.next = None
+                return save
+
+            prev = ptr
+            ptr = ptr.next
+
+    def getLen(self, head):
+        c = 0
+        ptr = head
+
+        while ptr:
+            c += 1
+            ptr = ptr.next
+        return c
+
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if k == 0 or head is None:
+            return head
+
+        k = k % self.getLen(head)
+
+        for i in range(k):
+            head = self.rotateByOne(head)
+        return head
+
+
+# Optimal
+# T.C. -> O(n)+O(n)
+# S.C. -> O(1)
+
+
+class Solution:
+    def getLen(self, head):
+        c = 0
+        ptr = head
+
+        while True:
+            if ptr.next is None:
+                return [c + 1, ptr]
+            c += 1
+            ptr = ptr.next
+
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if head is None or head.next is None or k == 0:
+            return head
+
+        ret = self.getLen(head)
+        length = ret[0]
+        lastNode = ret[1]
+        k = k % length
+
+        if k == 0:
+            return head
+
+        movePlaces = length - k
+        ptr = head
+        c = 1
+        save = None
+
+        while True:
+            if c == movePlaces:
+                save = ptr.next
+                ptr.next = None
+                break
+            c += 1
+            ptr = ptr.next
+
+        lastNode.next = head
+        return save
