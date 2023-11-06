f31268e7ced78502b224dd64816e2412305bdf9b
glowfi <86939591+glowfi@users.noreply.github.com>
2023-11-07 02:19:50 +0530

    added some subsets and backtracking programs

---
+++ Programs/5_Recursion/10_Generate_Parentheses.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/generate-parentheses/ , Medium
+
+# Brute
+# T.C. -O(N*2^N)
+# S.C  -O(N)
+
+
+class Solution:
+    def helper(self, openCount, closeCount, n, tmp, res):
+        if openCount == n and closeCount == n:
+            res.append(tmp)
+            return
+
+        if openCount < n:
+            self.helper(openCount + 1, closeCount, n, tmp + "(", res)
+
+        if closeCount < openCount:
+            self.helper(openCount, closeCount + 1, n, tmp + ")", res)
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        self.helper(0, 0, n, "", res)
+        return res
---
+++ Programs/5_Recursion/8_Letter_Combination_of_Phone_Number.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/ , Medium
+
+# https://0x0.st/Ht1D.799.png
+
+# Brute
+# T.C. - O(4^n)
+# S.C  - O(m)
+
+
+class Solution:
+    def helper(self, idx: int, processed: str, mp: dict, digits: str, res: list[str]):
+        if idx == len(digits):
+            res.append(processed)
+            return
+
+        currChar = digits[idx]
+        allCombi = mp[currChar]
+
+        for i in range(len(allCombi)):
+            tmp = allCombi[i]
+            self.helper(idx + 1, processed + tmp, mp, digits, res)
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        mp = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        res = []
+        self.helper(0, "", mp, digits, res)
+        return res
