66363b48cbb633492f710cb4e602dd614301b5b9
glowfi <86939591+glowfi@users.noreply.github.com>
2023-12-10 20:25:44 +0530

    added more tree programs

--- Programs/7_Trees/30_Right_View_of_a_Binary_Tree.py
+++ Programs/7_Trees/30_Right_View_of_a_Binary_Tree.py
@@ -2,5 +2,33 @@
 
 
 # Optimal
-# T.C. -
-# S.C  -
+# T.C. - O(n)
+# S.C  - O(1)
+
+from collections import deque
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
+        q = deque([root])
+        ans = []
+
+        while q:
+            lastNode = None
+            for i in range(len(q)):
+                currNode = q.popleft()
+
+                if currNode.left:
+                    q.append(currNode.left)
+
+                if currNode.right:
+                    q.append(currNode.right)
+
+                lastNode = currNode.val
+
+            ans.append(lastNode)
+
+        return ans
