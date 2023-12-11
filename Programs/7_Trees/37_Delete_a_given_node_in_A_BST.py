5d9f7ec56674dee5149e2001fa5455ffb8c2b781
glowfi <86939591+glowfi@users.noreply.github.com>
2023-12-11 17:19:14 +0530

    added some more tree programs

---
+++ Programs/7_Trees/37_Delete_a_given_node_in_A_BST.py
@@ -0,0 +1,11 @@
+#  https://leetcode.com/problems/delete-node-in-a-bst/ , Medium
+
+
+# Optimal
+# T.C. - O(log(n))
+# S.C  - O(log(n))
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        pass
