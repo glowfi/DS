2953ab9169bbb2caf1597606bc95d5ef84a206a3
glowfi <86939591+glowfi@users.noreply.github.com>
2023-12-17 04:05:06 +0530

    added more tree programs

--- Programs/7_Trees/28_Top_View_of_a_Binary_Tree.py
+++ Programs/7_Trees/28_Top_View_of_a_Binary_Tree.py
@@ -1,5 +1,34 @@
 # https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1 , Medium
 
 # Optimal
-# T.C. -
-# S.C  -
+# T.C. - O(n)+O(nlog(n))+O(n)
+# S.C  - O(n)
+
+
+class Solution:
+    def topView(self, root):
+        self.q = [[root, 0]]
+        self.tmp = {}
+        self.row = 0
+
+        def bfs():
+            while self.q:
+                for i in range(len(self.q)):
+                    node, col = self.q.pop(0)
+                    if col not in self.tmp:
+                        self.tmp[col] = [[self.row, node.data]]
+
+                    if node.left:
+                        self.q.append([node.left, col - 1])
+
+                    if node.right:
+                        self.q.append([node.right, col + 1])
+                self.row += 1
+
+        bfs()
+        ans = []
+
+        for i in sorted(self.tmp.items()):
+            ans.append(i[1][0][1])
+
+        return ans
