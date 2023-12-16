fa9e837bddfeb7ea46d72f4be6c2a3f8cfdeb5c4
glowfi <86939591+glowfi@users.noreply.github.com>
2023-12-17 02:57:09 +0530

    minor fix

--- Programs/7_Trees/57_LCA_in_BST.py
+++ Programs/7_Trees/57_LCA_in_BST.py
@@ -10,4 +10,13 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        pass
+        curr = root
+        while True:
+            if p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            elif p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            else:
+                break
+
+        return curr
