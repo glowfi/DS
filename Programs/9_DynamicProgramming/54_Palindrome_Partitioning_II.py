81a7bdb40c74d8708f3a4677b7af07c99c366ee8
glowfi <86939591+glowfi@users.noreply.github.com>
2024-03-08 14:47:03 +0530

    added some programs

--- Programs/9_DynamicProgramming/54_Palindrome_Partitioning_II.py
+++ Programs/9_DynamicProgramming/54_Palindrome_Partitioning_II.py
@@ -34,17 +34,128 @@ class Solution:
 
 
 # Recursion
-# T.C. -
-# S.C  -
+# T.C. - O(2^n)
+# S.C  - O(n)
+
+
+class Solution:
+    def checkPalindrome(self, s, i, j):
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+
+        return True
+
+    def solve(self, i, j, s):
+        if i >= j:
+            return 0
+
+        if self.checkPalindrome(s, i, j):
+            return 0
+
+        mn_cut = float("inf")
+
+        for k in range(i, j):
+            curr = self.solve(i, k, s) + self.solve(k + 1, j, s) + 1
+            if curr < mn_cut:
+                mn_cut = curr
+
+        return mn_cut
+
+    def minCut(self, s: str) -> int:
+        return self.solve(0, len(s) - 1, s)
+
 
 # Memoization
-# T.C. -
-# S.C  -
+# T.C. - O(N^2)
+# S.C  - O(N^3)+O(N)
+
+
+class Solution:
+    def checkPalindrome(self, s, i, j):
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+
+        return True
+
+    def solve(self, i, j, s, dp):
+        if i >= j:
+            return 0
+
+        if self.checkPalindrome(s, i, j):
+            return 0
+
+        if (i, j) in dp:
+            return dp[(i, j)]
+
+        mn_cut = float("inf")
+
+        for k in range(i, j):
+            if (i, k) in dp:
+                left = dp[(i, k)]
+            else:
+                left = self.solve(i, k, s, dp)
+                dp[(i, k)] = left
+
+            if (k + 1, j) in dp:
+                right = dp[(k + 1, j)]
+            else:
+                right = self.solve(k + 1, j, s, dp)
+                dp[(k + 1, j)] = right
+
+            curr = left + right + 1
+            if curr < mn_cut:
+                mn_cut = curr
+
+        dp[
+            (
+                i,
+                j,
+            )
+        ] = mn_cut
+
+        return mn_cut
+
+    def minCut(self, s: str) -> int:
+        return self.solve(0, len(s) - 1, s, {})
+
 
 # Tabulation
-# T.C. -
-# S.C  -
+# T.C. - O(N^3)
+# S.C  - O(N^3)
 
-# Space Optimized
-# T.C. -
-# S.C  -
+
+class Solution:
+    def checkPalindrome(self, s, i, j):
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            else:
+                i += 1
+                j -= 1
+
+        return True
+
+    def minCut(self, s: str) -> int:
+        dp = [[0 for _ in range(len(s))] for _ in range(len(s))]
+
+        for i in range(len(s), -1, -1):
+            for j in range(i + 1, len(s)):
+                if self.checkPalindrome(s, i, j):
+                    dp[i][j] = 0
+                else:
+                    mn_cut = float("inf")
+                    for k in range(i, j):
+                        curr = dp[i][k] + dp[k + 1][j] + 1
+                        if curr < mn_cut:
+                            mn_cut = curr
+                    dp[i][j] = mn_cut
+
+        return dp[0][len(s) - 1]
---
+++ Programs/9_DynamicProgramming/56_Evaluate_Expression_to_True_Boolean_Parenthesization.py
@@ -0,0 +1,13 @@
+# https://www.geeksforgeeks.org/problems/boolean-parenthesization5610/1 , Hard
+
+# Brute
+# T.C. -
+# S.C  -
+
+# Better
+# T.C. -
+# S.C  -
+
+# Optimal
+# T.C. -
+# S.C  -
