e54c820835b31198810ca11975989cd7145117dc
glowfi <86939591+glowfi@users.noreply.github.com>
2024-10-09 18:36:35 +0530

    added more stack programs

--- Programs/6_Stack&Queues/9_Min_Stack.py
+++ Programs/6_Stack&Queues/9_Min_Stack.py
@@ -1,35 +1,78 @@
 # https://leetcode.com/problems/min-stack/ , Medium
 
 
-# Optimal
+# Brute
 # T.C. - O(1) [push],O(1) [pop],O(1) [top],O(1) [getMin]
 # S.C  - O(n) [stack] + O(n) [hashmap]
-
-from collections import deque
 
 
 class MinStack:
 
     def __init__(self):
-        self.stk = deque([])
-        self.min_map = {}
+        self.stk = []
+        self.minMap = {}
 
     def push(self, val: int) -> None:
-        if len(self.stk) == 0:
-            self.min_map[0] = val
+        currIdx = len(self.stk) - 1
+        if currIdx == -1:
+            self.minMap[0] = val
         else:
-            self.min_map[len(self.stk)] = min(val, self.min_map[len(self.stk) - 1])
+            self.minMap[currIdx + 1] = min(val, self.minMap[currIdx])
 
         self.stk.append(val)
 
     def pop(self) -> None:
-        if not len(self.stk) == 0:
-            self.stk.pop()
+        if len(self.stk) > 0:
+            self.stk.pop(-1)
+
+    def top(self) -> int:
+        if len(self.stk) <= 0:
+            return -1
+        return self.stk[-1]
+
+    def getMin(self) -> int:
+        if len(self.stk) <= 0:
+            return -1
+        lastIdx = len(self.stk) - 1
+        return self.minMap[lastIdx]
+
+
+# Brute
+# T.C. - O(1) [push],O(1) [pop],O(1) [top],O(1) [getMin]
+# S.C  - O(n) [stack]
+
+
+class MinStack:
+
+    def __init__(self):
+        self.stk: list[int] = []
+        self.prevMin = float("inf")
+
+    def push(self, val: int) -> None:
+        if len(self.stk) == 0:
+            self.prevMin = val
+            self.stk.append(val)
+        else:
+            if val < self.prevMin:
+                newVal = 2 * val - self.prevMin
+                self.stk.append(newVal)
+                self.prevMin = val
+            else:
+                self.stk.append(val)
+
+    def pop(self) -> None:
+        if len(self.stk) > 0:
+            top = self.stk.pop(-1)
+            if top < self.prevMin:
+                self.prevMin = 2 * self.prevMin - top
 
     def top(self) -> int:
         if len(self.stk) > 0:
-            return self.stk[-1]
+            top = self.stk[-1]
+            if top < self.prevMin:
+                return self.prevMin
+            return top
         return -1
 
     def getMin(self) -> int:
-        return self.min_map[len(self.stk) - 1]
+        return self.prevMin
