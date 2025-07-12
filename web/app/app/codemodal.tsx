53329cf0da97e630f8c54967a8ecd8546d6b9be7
glowfi <86939591+glowfi@users.noreply.github.com>
2025-07-12 09:11:41 +0530

    improve responsiveness of code modal

--- web/app/app/codemodal.tsx
+++ web/app/app/codemodal.tsx
@@ -25,121 +25,123 @@ interface CodeModalProps {
     onClose: () => void;
 }
 
-function approachesToObject(approaches: Approach[]): {
-    [key: string]: Approach;
-} {
+const mapApproachesToObject = (
+    approaches: Approach[]
+): Record<string, Approach> => {
     return approaches.reduce(
-        (obj, approach) => ({ ...obj, [approach.type]: approach }),
-        {}
+        (acc, approach) => {
+            acc[approach.type] = approach;
+            return acc;
+        },
+        {} as Record<string, Approach>
     );
-}
+};
 
-const CodeModal: React.FunctionComponent<CodeModalProps> = ({
+const CodeModal: React.FC<CodeModalProps> = ({
     question,
     modalOpen,
     onClose
 }) => {
-    if (question === undefined) {
+    if (!question) {
         return null;
     }
 
-    const defaultSelectedAppraoch =
+    const defaultSelectedApproach =
         question.approaches.length > 0 ? question.approaches[0].type : 'NA';
     const [selectedApproach, setSelectedApproach] = useState(
-        defaultSelectedAppraoch
+        defaultSelectedApproach
     );
-    const appToObject = approachesToObject(question.approaches);
+    const approachesMap = mapApproachesToObject(question.approaches);
 
     return (
         <Sheet open={modalOpen} onOpenChange={onClose}>
             <SheetTrigger asChild className="hidden">
                 <Button variant="outline">See Code</Button>
             </SheetTrigger>
-            <SheetContent className="h-screen w-screen overflow-auto">
+            <SheetContent className="h-screen w-screen overflow-auto p-4 md:p-8">
                 <SheetHeader>
                     <SheetTitle>
                         <Link
                             target="_blank"
                             href={question.problem_link}
-                            className="hover:underline"
+                            className="hover:underline flex items-center gap-1"
                         >
-                            <div className="flex-row items-center gap-1">
-                                <span>{question.problem_name}</span>
-                                <SquareArrowOutUpRight className="h-4 w-4" />
-                            </div>
+                            <span>{question.problem_name}</span>
+                            <SquareArrowOutUpRight className="h-4 w-4" />
                         </Link>
                     </SheetTitle>
                 </SheetHeader>
-                <div className="flex flex-col mt-6 overflow-auto gap-6 justify-between">
+                <div className="flex flex-col mt-6 overflow-auto gap-6">
                     <div className="flex flex-col">
-                        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                        <h3 className="text-2xl font-semibold tracking-tight">
                             Problem Statement
                         </h3>
-                        <pre>{question.question}</pre>
+                        <pre className="whitespace-pre-wrap">
+                            {question.question}
+                        </pre>
                     </div>
 
                     <div className="flex flex-col">
-                        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                        <h3 className="text-2xl font-semibold tracking-tight">
                             Intuition
                         </h3>
-                        <pre>{appToObject[selectedApproach].intuition}</pre>
+                        <pre className="whitespace-pre-wrap">
+                            {approachesMap[selectedApproach].intuition}
+                        </pre>
                     </div>
 
-                    <Tabs defaultValue={defaultSelectedAppraoch}>
+                    <Tabs defaultValue={defaultSelectedApproach}>
                         <TabsList>
-                            {question.approaches.map((a, idx) => {
-                                return (
-                                    <TabsTrigger
-                                        value={`${a.type}-${idx}`}
-                                        key={idx}
-                                        onClick={() => {
-                                            setSelectedApproach(a.type);
-                                        }}
-                                    >
-                                        {a.type}
-                                    </TabsTrigger>
-                                );
-                            })}
+                            {question.approaches.map((approach, index) => (
+                                <TabsTrigger
+                                    value={`${approach.type}-${index}`}
+                                    key={index}
+                                    onClick={() =>
+                                        setSelectedApproach(approach.type)
+                                    }
+                                >
+                                    {approach.type}
+                                </TabsTrigger>
+                            ))}
                         </TabsList>
 
                         <div className="flex flex-col">
-                            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                            <h3 className="text-2xl font-semibold tracking-tight">
                                 Approaches
                             </h3>
-                            {question.approaches.map((a, idx) => {
-                                return (
-                                    <TabsContent
-                                        value={`${a.type}-${idx}`}
-                                        key={idx}
+                            {question.approaches.map((approach, index) => (
+                                <TabsContent
+                                    value={`${approach.type}-${index}`}
+                                    key={index}
+                                >
+                                    <SyntaxHighlighter
+                                        wrapLines={true}
+                                        language={question.language}
+                                        style={gruvboxDark}
                                     >
-                                        <SyntaxHighlighter
-                                            wrapLines={true}
-                                            language={question.language}
-                                            style={gruvboxDark}
-                                        >
-                                            {a.code}
-                                        </SyntaxHighlighter>
-                                    </TabsContent>
-                                );
-                            })}
+                                        {approach.code}
+                                    </SyntaxHighlighter>
+                                </TabsContent>
+                            ))}
                         </div>
+
                         <div className="flex flex-col">
-                            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                            <h3 className="text-2xl font-semibold tracking-tight">
                                 Time Complexity
                             </h3>
-                            <Badge>{appToObject[selectedApproach].tc}</Badge>
+                            <Badge>{approachesMap[selectedApproach].tc}</Badge>
                         </div>
                         <div className="flex flex-col">
-                            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                            <h3 className="text-2xl font-semibold tracking-tight">
                                 Space Complexity
                             </h3>
-                            <Badge>{appToObject[selectedApproach].sc}</Badge>
+                            <Badge>{approachesMap[selectedApproach].sc}</Badge>
                         </div>
                     </Tabs>
                 </div>
                 <SheetFooter className="mt-3">
                     <SheetClose asChild>
-                        <Button type="submit" onClick={onClose}>
+                        <Button type="button" onClick={onClose}>
                             Close
                         </Button>
                     </SheetClose>
