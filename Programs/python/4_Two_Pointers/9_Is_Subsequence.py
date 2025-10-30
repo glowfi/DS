0ad12a65f61a10f8b15a9aecfc810fb58eacb0de
glowfi <86939591+glowfi@users.noreply.github.com>
2025-10-30 20:05:41 +0530

    update json

--- Programs/python/4_Two_Pointers/9_Is_Subsequence.py
+++ Programs/python/4_Two_Pointers/9_Is_Subsequence.py
@@ -2,9 +2,7 @@
 
 # Question
 # Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
-
 # A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
-
 
 # Example 1:
 
@@ -17,15 +15,12 @@
 
 
 # Constraints:
-
 # 0 <= s.length <= 100
 # 0 <= t.length <= 10^4
 # s and t consist only of lowercase English letters.
 
-
 # Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109,
 # and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
-
 
 # Optimal
 # T.C. - O(n)
@@ -52,7 +47,7 @@ class Solution:
         return p1 == len(s)
 
 
-# Optimal (Followup)
+# Optimal
 # T.C. - O(t)+ O(|s| × log(|t|))*k
 # S.C  - O(t)
 
